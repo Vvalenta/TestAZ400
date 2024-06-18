17c1782504ed7fce1bf8f3126119982f39c036a5
Vvalenta <valentav@email.cz>
2024-06-18 14:19:51 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System
+
+namespace AZ400.Test 
+{
+    static class Program 
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
